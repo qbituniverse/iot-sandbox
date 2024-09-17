c4fc4b32b2873e6fe4702feb295beb820ba8139f
Marcin Narloch <marcin.narloch@qbituniverse.com>
2024-09-17 22:22:49 +0200

    Init

---
+++ IoT.Sandbox.Console/Hcsr501/Hcsr501V1.cs
@@ -0,0 +1,42 @@
+﻿using System.Device.Gpio;
+using Iot.Device.Hcsr501;
+using Microsoft.Extensions.Logging;
+
+namespace IoT.Sandbox.Console.Hcsr501;
+
+internal class Hcsr501V1 : IRun
+{
+    private const int Output = 21;
+
+    private readonly ILogger<Hcsr501V1> _logger;
+
+    public Hcsr501V1(ILogger<Hcsr501V1> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Run()
+    {
+        var hcsr501 = new Iot.Device.Hcsr501.Hcsr501(Output);
+        hcsr501.Hcsr501ValueChanged += SensorValueChanged;
+        _logger.LogInformation("Motion Start");
+
+        void SensorValueChanged(object sender, Hcsr501ValueChangedEventArgs args)
+        {
+            if (args.PinValue == PinValue.High)
+            {
+                _logger.LogInformation("Motion Detected");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                _logger.LogInformation("Motion NOT Detected");
+            }
+        }
+
+        while (true)
+        {
+        }
+        // ReSharper disable once FunctionNeverReturns
+    }
+}
---
+++ IoT.Sandbox.Console/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using IoT.Sandbox.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile($"appsettings{(string.IsNullOrEmpty(environment) ? "" : "." + environment)}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+IConfiguration configuration = configBuilder.Build();
+
+var consoleConfiguration = configuration.GetSection(nameof(ConsoleConfiguration)).Get<ConsoleConfiguration>()!;
+
+var loggerConfiguration = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .Enrich.WithProcessId()
+    .Enrich.WithProcessName()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithEnvironmentUserName()
+    .WriteTo.Console(
+        restrictedToMinimumLevel: Enum.Parse<LogEventLevel>(consoleConfiguration.LogLevel!.Console!),
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} <s:{SourceContext}>{NewLine}{Exception}",
+        theme: AnsiConsoleTheme.Code);
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        Register.Services(consoleConfiguration, services);
+    })
+    .ConfigureLogging(cfg =>
+    {
+        cfg.ClearProviders();
+        cfg.AddSerilog(loggerConfiguration.CreateLogger());
+    })
+    .Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+AppDomain.CurrentDomain.UnhandledException += Error;
+
+logger.LogInformation(@"IoT.Sandbox.Console Configuration {Config}",
+    JsonSerializer.Serialize(consoleConfiguration, new JsonSerializerOptions { WriteIndented = true }));
+
+await Task.Run(host.Services.GetService<IRun>()!.Run);
+
+void Error(object sender, UnhandledExceptionEventArgs e)
+{
+    var exception = e.ExceptionObject as Exception;
+
+    Console.WriteLine(
+        $"Error: Message {exception!.Message} Source {exception.Source} StackTrace {exception.StackTrace}");
+
+    Thread.Sleep(15000);
+
+    logger.LogError(@"Error: Message {Message} Source {Source} StackTrace {StackTrace}",
+        exception.Message, exception.Source, exception.StackTrace);
+    Thread.Sleep(15000);
+
+    Environment.Exit(-1);
+}
